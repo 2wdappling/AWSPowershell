f28e28cda3ac30048f397205a9bcb5c55ba3a032
Drew Appling <dappling@2ndwatch.com>
2018-07-27 06:33:21 -0700

    Testing Get-AWSRegion

--- AWS Find Instance.ps1
+++ AWS Find Instance.ps1
@@ -14,7 +14,7 @@ Set-AWSCredential -AccessKey $aws_access_key_id -SecretKey $aws_secret_access_ke
 
 #Need to find a way to pull this info from AWS as new instances will require manual addition
 $Regions = @("us-east-1", "us-east-2", "us-west-1", "us-west-2", "ap-northeast-1", "ap-northeast-2", "ap-northeast-3", "ap-south-1", "ap-southeast-1", "ap-southeast-2", "ca-central-1", "cn-north-1", "cn-northwest-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-west-3","sa-east-1")
-
+#(Get-AWSRegion).Name
 #InstanceID to search for
 $InstanceID = "insertIDhere"
 
